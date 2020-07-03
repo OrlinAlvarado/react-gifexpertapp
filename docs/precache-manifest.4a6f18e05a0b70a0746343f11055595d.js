7334f88b85816e77a990ced628d2485605778f14
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-07-02 21:34:01 -0600

    Docs agregado

---
+++ docs/precache-manifest.4a6f18e05a0b70a0746343f11055595d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9da894c16ae44026be55b107f667b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c30f2929074e967315",
+    "url": "/static/css/main.49ff03ad.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c30f2929074e967315",
+    "url": "/static/js/main.5a8a678e.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
